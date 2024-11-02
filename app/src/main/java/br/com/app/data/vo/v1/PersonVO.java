aeeeea9f4a8e8105263becb190e27afb6d85ed9a
Nicolas Vasca <nicolasvasca@gmail.com>
2024-11-02 04:41:07 -0300

    feat: json order example

--- app/src/main/java/br/com/app/data/vo/v1/PersonVO.java
+++ app/src/main/java/br/com/app/data/vo/v1/PersonVO.java
@@ -2,14 +2,22 @@ package br.com.app.data.vo.v1;
 
 import java.io.Serializable;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+@JsonPropertyOrder({"id", "address", "first_name", "last_name", "gender"})
 public class PersonVO implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 
 	private Long id;
+	@JsonProperty("first_name")
 	private String firstName;
+	@JsonProperty("last_name")
 	private String lastName;
 	private String address;
+	@JsonIgnore
 	private String gender;
 	
 	public PersonVO() {}
